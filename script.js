ede7f170eaef9f868c69d21c21f578df31a9befa
fullgitstack <mackormack0@gmail.com>
2025-10-06 22:09:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const body1 = document.querySelector("body")
 
 const ifr = document.createElement("iframe")
-const ifr.src = "http://challenge01.root-me.org/web-client/ch23/index.php?action=profile"
+ifr.src = "http://challenge01.root-me.org/web-client/ch23/index.php?action=profile"
 body1.appendChild(ifr)
 
