2c632320859120739919ecfa4c4b29bf3069de3e
fullgitstack <mackormack0@gmail.com>
2025-10-06 22:25:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const ifr = document.createElement("iframe")
 ifr.src = "http://challenge01.root-me.org/web-client/ch23/index.php?action=profile"
 body1.appendChild(ifr)
 
-const token = ifr.contentDocument.body.querySelector("#token")
-console.log(token)
+setTimeout(() => {
+  const token = ifr.contentWindow.document.querySelector("#token")
+  console.log(token)
+}, 3000)
 
