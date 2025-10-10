d39e4e90b1d5b8570bcaeec879d71c222cff5477
fullgitstack <mackormack0@gmail.com>
2025-10-10 18:45:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
+
+
+document.addEventListener("DOMContentLoaded", () => {
 const ifr = document.querySelector("#principalIframe");
-
-ifr.addEventListener("load", () => {
-
 ifr.contentDocument.querySelector("#username").value = "test1"
 ifr.contentDocument.querySelector("#status").checked = true
 
